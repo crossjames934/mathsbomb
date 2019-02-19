b8860056c4e6238f3efc3f91dc2681a66d7338e0
crossjames934 <crossjames934@gmail.com>
2019-02-19 13:09:05 +0000

    host section started

---
+++ public/host.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(groupName, id, pin, entryPassword) {
+        this.groupName = groupName;
+        this.id = id;
+        this.pin = pin;
+        this.entryPassword = entryPassword;
+    }
+}
--- server.js
+++ server.js
@@ -1,22 +1,71 @@
-// server.js
-// where your node app starts
-
-// init project
+// Dependencies
 const express = require('express');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const bcrypt = require('bcrypt');
+const socket = require('socket.io');
+const shortid = require('shortid');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// Connect to Database
+mongoose.connect(process.env.DB);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log("Connected to Database! :D");
+});
 
-// http://expressjs.com/en/starter/static-files.html
+// Schema and Models
+const roomSchema = new mongoose.Schema({
+    groupName: String,
+    id: String,
+    pin: String,
+    entryPassword: String,
+    unixTimestamp: Number
+});
+const Room = mongoose.model('Room', roomSchema);
+
+// Server files and basic app usage
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Home page
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+// Routes
+app.route('/hostSetup/')
+    .post((req, res) => {
+        // Get all existing IDs of rooms to make sure we don't generate a duplicate one
+        Room.find((err, docs) => {
+            if (err) return console.error(err);
+            const existingIDs = docs.map(doc => doc.id);
+            const pinMultiplier = 1000000;
+            // Keep generating new number until we get one that doesn't already exist
+            let rndID = Math.floor(Math.random() * pinMultiplier * 0.9 + pinMultiplier * 0.1);
+            while (existingIDs.indexOf(rndID) > -1) {
+                rndID = Math.floor(Math.random() * pinMultiplier * 0.9 + pinMultiplier * 0.1);
+            }
+            const newRoom = new Room({
+                groupName: req.body.groupName,
+                id: shortid.generate(),
+                pin: rndID,
+                entryPassword: req.body.entryPassword,
+                unixTimestamp: new Date().getTime()
+            });
+            newRoom.save((err, room) => {
+                if (err) return console.error(err);
+                return res.json(room);
+            });
+        });
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, () => {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
